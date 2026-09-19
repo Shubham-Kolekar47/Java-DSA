ce52daa2a93fc369810aaadd520f2c6727302419
shubham kolekar <shubhamkolekar216@gmail.com>
2026-09-19 12:53:15 +0530

    changes

--- exp217.java
+++ exp217.java
@@ -1,5 +1,4 @@
-import org.w3c.dom.Node;
-
+import java.util.LinkedList;
 public class exp217 {//linked list
     public static class Node{
         int data;
@@ -270,6 +269,60 @@ public class exp217 {//linked list
         //remove cycle -> last.next = null
         prev.next = null;
     }
+
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast !=null && head.next !=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //mid node
+    }
+
+    private Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 !=null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head){
+        if(head == null || head.next != null){
+            return head;
+        }
+        //find mid
+        Node mid = getMid(head);
+        //left and right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        //merge
+        return merge(left,right);
+    }
     public static void main(String args[]){
         // exp217 ll = new exp217();
         // ll.addFirst(2);
@@ -300,14 +353,24 @@ public class exp217 {//linked list
         // ll.removeLast();
         // ll.print();
         // System.out.println(ll.size);
-        head = new Node(1);
-        Node temp = new Node(2);
-    head.next = temp;
-    head.next.next = new Node(3);
-    head.next.next.next = temp;
+    //     head = new Node(1);
+    //     Node temp = new Node(2);
+    // head.next = temp;
+    // head.next.next = new Node(3);
+    // head.next.next.next = temp;
     //1->2->3->1
-    System.out.println(isCycle());
-    removeCycle();
-    System.out.println(isCycle());
+    // System.out.println(isCycle());
+    // removeCycle();
+    // System.out.println(isCycle());
+    LinkedList ll = new LinkedList();
+    ll.addFirst(1);
+    ll.addFirst(2);
+    ll.addFirst(3);
+    ll.addFirst(4);
+    ll.addFirst(5);
+
+    ll.print();
+    ll.head = ll.mergeSort(ll.head);
+    ll.print();
     }
 }
