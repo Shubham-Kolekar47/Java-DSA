fe053203a8744567be7db1e718f8164905c623a0
Shubham-Kolekar47 <shubhamkolekar216@gmail.com>
2026-09-22 19:49:23 +0530

    Fix formatting in numTri method

--- exp100.java
+++ exp100.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class exp100{
     public static void numTri(int n) {
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
+            for(int j=1;j <=n-i;j++){
                 System.out.print( " ");
             }
             for(int j=1;j<=i;j++){
