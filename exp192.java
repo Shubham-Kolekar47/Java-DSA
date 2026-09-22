5350c5848bfe1e59b034153e571ba6e29a5009c6
shubham kolekar <shubhamkolekar216@gmail.com>
2026-09-22 19:40:14 +0530

    changes

--- exp192.java
+++ exp192.java
@@ -17,7 +17,7 @@ public class exp192 {
     public static void printBoard(char board[][]){
         System.out.println("--------chess board--------");
         for(int i = 0; i < board.length; i++){
-            for(int j = 0;j < board.length;j++){
+            for(int j = 0 ;j < board.length;j++){
                 System.out.println(board[i][j]+ " ");
             }
             System.out.println();
