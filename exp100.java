8b5868fc8afb67f20b53180941290e26131123d9
Shubham-Kolekar47 <shubhamkolekar216@gmail.com>
2026-09-22 19:49:40 +0530

    Fix loop condition for printing spaces

--- exp100.java
+++ exp100.java
@@ -8,7 +8,7 @@ public class exp100{
             for(int j=1;j<=i;j++){
                 System.out.print(i + " ");
             }
-            for(int j=1;j<=n-i;j++){
+            for(int j=1;j <=n-i;j++){
                 System.out.print(" ");
             }
             System.out.println();
